d6c28965f9248d54e2219a20fbe874cdc741b8da
Donald Lee <donaldlee50@gmail.com>
2021-08-30 20:59:24 -0400

    Working scripts for kovan

---
+++ scripts/deploy_main_aave.js
@@ -0,0 +1,19 @@
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  const AaveV2FlashLoan = await ethers.getContractFactory("AaveV2FlashLoan");
+  const aave_contract = await AaveV2FlashLoan.deploy('0x9aab3f75489902f3a48495025729a0af77d4b11e','0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F','0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5');
+
+  console.log("Aave Contract Token address:", aave_contract.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
